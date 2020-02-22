6daabfa3c0e2207078fc411319c4c58802c16b57
thegreated <edwardarilla@gmail.com>
2020-02-22 14:36:44 +0800

    lol

---
+++ resources/views/users/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts/contentLayoutMaster') <!--// choose any one option {contentLayoutMaster/fullLayoutMaster}-->
+
+@section('title', 'User') <!-- // change title accrodingly -->
+
+@section('content')
+    <div>
+       test
+    </div>
+@endsection
